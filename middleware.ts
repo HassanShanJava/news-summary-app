dcd45690799492009bf6bf023815ef081c50f9f3
HassanShanJava <hassanshan675@gmail.com>
2023-09-17 06:18:18 +0500

    added new middleware and relations

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,14 @@
 import { authMiddleware, withClerkMiddleware } from "@clerk/nextjs";
-import { NextRequest, NextResponse } from "next/server";
+// import { NextRequest, NextResponse } from "next/server";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
-// export default authMiddleware({});
-
-export default withClerkMiddleware((req: NextRequest) => {
-      return NextResponse.next();
+export default authMiddleware({
+      ignoredRoutes: ["/((?!api|trpc))(_next|.+\..+)(.*)", "/api/webhooks/user"]
 });
+
+
 export const config = {
       matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
