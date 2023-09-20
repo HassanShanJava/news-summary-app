28536a5d7f35d716e21b73b021a1752f07bd6952
HassanShanJava <hassanshan675@gmail.com>
2023-09-20 06:58:58 +0500

    optimized filtering of news list

--- components/home/category.tsx
+++ components/home/category.tsx
@@ -12,16 +12,12 @@ const Category = () => {
   useEffect(() => {
     (async () => {
       const res = await fetchNewsApi(category);
-      setNews(res.articles);
+      const resFiltered=res.articles.filter((item:any) => item.url !== 'https://removed.com')
+      setNews(resFiltered);
     })();
   }, [category]);
   console.log({ news });
 
-  // const { data, isFetchingNextPage, fetchNextPage, hasNextPage } = useInfiniteQuery<any[]>({
-  //   queryKey: ['newsList'],
-  //   queryFn: fetchNewsApi,
-  //   getNextPageParam: (_, pages) => pages.length + 1,
-  // });
 
   return (
     <div className="w-full px-8 sm:px-16 py-2 sm:py-4">
@@ -31,21 +27,25 @@ const Category = () => {
             onClick={() => {
               setCategory(item.type);
             }}
-            className={`cursor-pointer rounded-full shadow-md px-2 py-1 sm:px-4 sm:py-2 font-semibold sm:font-bold text-sm sm:text-base bg-orange-100 hover:bg-orange-400 text-orange-400 hover:text-orange-100`}
+            className={`cursor-pointer rounded-full shadow-md px-2 py-1 sm:px-4 sm:py-2 font-semibold sm:font-bold text-sm sm:text-base ${item.type == category ? "bg-orange-400 text-orange-100" : "bg-orange-100 text-orange-400"} hover:bg-orange-400  hover:text-orange-100`}
           >
             {item.title}
           </div>
         ))}
       </div>
-      {news && (
+      {news && news.length > 0 ? (
         <ul className="w-full my-6 grid grid-cols-1 sm:grid-cols-2 ">
           {/* need to remove urls that are do not return data */}
-          {news.filter((item)=>item.url!=='https://removed.com').map((newsItem, i) => (
+          {news.map((newsItem, i) => (
             <li key={i} className="py-4">
-              <NewsItem data={newsItem} />
+              <NewsItem data={newsItem} length={news.length} i={i} />
             </li>
           ))}
         </ul>
+      ) : (
+        <div className="flex justify-center items-center h-[70vh]">
+          <p className="text-xl sm:text-3xl font-bold">No Articles Found</p>
+        </div>
       )}
     </div>
   );
